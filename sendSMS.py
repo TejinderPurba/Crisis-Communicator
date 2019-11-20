9b9757b15fbb59729fe29e3819ef706ec1a0a76c
TejinderPurba <rikkupurba@gmail.com>
2019-11-20 11:17:46 -0500

    Changed message, sender, and recipient to be dynamic

--- sendSMS.py
+++ sendSMS.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 
-sendSMS (str account_sid, str auth_token, str sender, str recipient) {
+sendSMS (str account_sid, str auth_token, str sender, str recipient, str msgToSend) {
 
     client = Client(account_sid, auth_token)
 
     message = client.messages \
                 .create(
-                     body="A person's life may be at risk! Please send emergency services to their house.",
-                     from_='+12262711202',
-                     to='+15192774981'
+                     body=msgToSend,
+                     from_=sender,
+                     to=recipient
                  )
     print(message.sid)
 }
