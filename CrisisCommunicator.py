92dd5b01d056987d86879ccfd3e003f0a4ecf1d2
TejinderPurba <rikkupurba@gmail.com>
2019-11-21 10:47:45 -0500

    Removed serial port configuration

--- CrisisCommunicator.py
+++ CrisisCommunicator.py
@@ -17,20 +17,20 @@ try:
         bpm = p.BPM
         voltage = p.voltage
         if bpm > 0:
-            print("BPM: %d" % bpm)
+            print("BPM: %d\n" % bpm)
         else:
-            print("No Heartbeat found")
+            print("No Heartbeat found\n")
         if not msgSent and voltage > 0:
-        	print("SENDING EMERGENCY NOTIFICATIONS")
+        	print("SENDING EMERGENCY NOTIFICATIONS\n")
         	sendSMS(conf_list['sms_account_sid'], conf_list['sms_auth_token'], conf_list['sms_sender_number'], conf_list['sms_sender_recipient'], conf_list['sms_message'])
 			if (conf_list['send_pushbullet_option']):
 				sendPushBulletNotification(conf_list['pushbullet_api_key'], conf_list['pushbullet_message'])
 			if (conf_list['send_email_option']):
 				sendPushBulletEmail(conf_list['pushbullet_api_key'], conf_list['pushbullet_message'], conf_list['pushbullet_email'])
+            print("EMERGENCY NOTIFICATIONS SENT\n")
 			msgSent = True
 			msgSentTime = time.time()
         time.sleep(1)
 except:
     p.stopAsyncBPM()
 
-ser.close()
