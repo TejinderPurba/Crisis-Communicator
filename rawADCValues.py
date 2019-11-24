c5677d3b374754a1d4c9bc7fe7ef26072666b4b0
TejinderPurba <rikkupurba@gmail.com>
2019-11-24 15:54:59 -0500

    Removed template code

--- rawADCValues.py
+++ rawADCValues.py
@@ -10,15 +10,6 @@ xbee = ZigBee(ser, escaped=True)
 
 print ('Starting up ADC Reader')
 
-#xbee.remote_at(
-#    dest_addr=b'\x1d\x14',
-#    command='D1',
-#    parameter=b'\x04')
-
-#xbee.remote_at(
-#    dest_addr=b'\x1d\x14',
-#    command='WR')
-
 while True:
 	try:
 		response = xbee.wait_read_frame()
