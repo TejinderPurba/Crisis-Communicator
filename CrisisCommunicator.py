9a07fd1b10ff0aba4b3afbc5e3c3288e1ccee113
Tejinder Purba <rikkupurba@gmail.com>
2019-11-24 18:22:28 -0500

    Fixed syntax, added message clearing

--- CrisisCommunicator.py
+++ CrisisCommunicator.py
@@ -14,77 +14,84 @@ data.startAsyncTemp()
 
 conf_list = None
 with open(r'Configuration.yaml') as file:
-    conf_list = yaml.full_load(file)
+	conf_list = yaml.full_load(file)
 
 try:
 	while True:
+		time.sleep(1)
 		bpm = data.BPM
 		voltage = data.pulseVoltage
-        temp = data.temp
-        motion = data.motion
-        bpmQueueSize = data.bpmQueue.qsize()
-        tempQueueSize = data.tempQueue.qsize()
+		temp = data.temp
+		motion = data.motion
+		bpmQueueSize = data.bpmQueue.qsize()
+		tempQueueSize = data.tempQueue.qsize()
 
 		if bpm > 0:
 			print("BPM: %d\n" % bpm)
 		else:
 			print("NO HEARTBEAT DETECTED\n")
+			
+		if temp > -100:
+			print("TEMPERATURE: %d\n" % temp)
+		else:
+			print("NO TEMPERATURE DETECTED\n")
 
-        if temp > -100:
-            print("TEMPERATURE: %d\n" % temp)
-        else:
-            print("NO TEMPERATURE DETECTED\n")
+		if motion:
+			print("DEVICE MOTION DETECTED\n\n")
+		else:
+			print("NO DEVICE MOTION DETECTED\n\n")
 
-        if motion:
-            print("DEVICE MOTION WAS DETECTED\n\n")
-        else:
-            print("DEVICE MOTION WAS NOT DETECTED\n\n")
-
-		if not msgSent and voltage > 0:
-			print("SENDING EMERGENCY NOTIFICATIONS\n")
-            sendNotifications(conf_list['sms_message'], conf_list['pushbullet_message'])
-            print("EMERGENCY NOTIFICATIONS SENT\n")
+		if not msgSent and (voltage > 0 or motion == True):
+			print("++++++++++++++++++++++++++++++++++++++++++++++\nSENDING EMERGENCY NOTIFICATIONS\n++++++++++++++++++++++++++++++++++++++++++++++\n")
+			#sendNotifications(conf_list['sms_message'], conf_list['pushbullet_message'])
+			print("++++++++++++++++++++++++++++++++++++++++++++++\nEMERGENCY NOTIFICATIONS SENT\n++++++++++++++++++++++++++++++++++++++++++++++\n\n")
 			msgSent = True
 			msgSentTime = time.time()
 
-        if msgSent and time.time() - msgSentTime >= 5:          # change to 30 seconds when done testing
-            vitalsCheck = False
-            sms_msg = "VITALS WERE CAPTURED\n\nBPM: %d\nTEMPERATURE: %d\nDEVICE IS IN MOTION" % (bpm, temp)
-            pb_msg = "VITALS WERE CAPTURED\n\nBPM: %d\nTEMPERATURE: %d\nDEVICE IS IN MOTION" % (bpm, temp)
-            if bpm > 0:
-                sms_msg.join("BPM: %d\n" % bpm)
-                pb_msg.join("BPM: %d\n" % bpm)
-                vitalsCheck = True
-            else:
-                sms_msg.join("BPM NOT DETECTED")
-                pb_msg.join("BPM NOT DETECTED")
-            if temp > -100:
-                sms_msg.join("TEMPERATURE: %d\n" % temp)
-                pb_msg.join("TEMPERATURE: %d\n" % temp)
-                vitalsCheck = True
-            else:
-                sms_msg.join("TEMPERATURE WAS NOT DETECTED")
-                pb_msg.join("TEMPERATURE WAS NOT DETECTED")
-            if motion:
-                sms_msg.join("DEVICE MOTION WAS DETECTED")
-                pb_msg.join("DEVICE MOTION WAS DETECTED")
-                vitalsCheck = True
-            else:
-                sms_msg.join("DEVICE MOTION WAS NOT DETECTED")
-                pb_msg.join("DEVICE MOTION WAS NOT DETECTED")
-            if vitalsCheck:
-                sendNotifications(sms_msg, pb_msg)
-            vitalsCheck = False 
-            msgSentTime = 0
+		timeVal = time.time() - msgSentTime
 
-        if msgSent and bpmQueueSize == 0 and tempQueueSize == 0:            # Approximation when XBee is sent to sleep mode
-            msgSent = False
-
-		time.sleep(1)
+		if msgSentTime > 0 and (timeVal >= 5.0):          # change to 30 seconds when done testing
+			vitalsCheck = False
+			sms_msg = "VITALS WERE CAPTURED\n\n"
+			pb_msg = "VITALS WERE CAPTURED\n\n"
+			if bpm > 0:
+				sms_msg.join("BPM: %d\n" % bpm)
+				pb_msg.join("BPM: %d\n" % bpm)
+				vitalsCheck = True
+			else:
+				sms_msg.join("BPM NOT DETECTED\n")
+				pb_msg.join("BPM NOT DETECTED\n")
+			if temp > -100:
+				sms_msg.join("TEMPERATURE: %d\n" % temp)
+				pb_msg.join("TEMPERATURE: %d\n" % temp)
+				vitalsCheck = True
+			else:
+				sms_msg.join("TEMPERATURE WAS NOT DETECTED\n")
+				pb_msg.join("TEMPERATURE WAS NOT DETECTED\n")
+			if motion:
+				sms_msg.join("DEVICE MOTION WAS DETECTED\n")
+				pb_msg.join("DEVICE MOTION WAS DETECTED\n")
+				vitalsCheck = True
+			else:
+				sms_msg.join("DEVICE MOTION WAS NOT DETECTED\n")
+				pb_msg.join("DEVICE MOTION WAS NOT DETECTED\n")
+			if vitalsCheck:
+				print("++++++++++++++++++++++++++++++++++++++++++++++\nSENDING VITALS NOTIFICATION\n++++++++++++++++++++++++++++++++++++++++++++++\n")
+				#sendNotifications(sms_msg, pb_msg)
+				print("++++++++++++++++++++++++++++++++++++++++++++++\nVITALS NOTIFICATION SENT\n++++++++++++++++++++++++++++++++++++++++++++++\n\n")
+			vitalsCheck = False 
+			msgSentTime = 0
+		
+		msgRxTime = data.msgRxTime
+		if (time.time() - msgRxTime > 20) and msgRxTime > 0:            # Approximation when XBee is sent to sleep mode, change to 30+ after testing
+			msgSent = False
+			data.clearValues()
+			time.sleep(1)
+		
 except:
-    data.stopAsyncXBee()
-    data.stopAsyncBPM()
-    data.stopAsyncTemp()
+	data.stopAsyncXBee()
+	data.stopAsyncBPM()
+	data.stopAsyncTemp()
 
 def sendNotifications(sms_msg, pb_msg):
     sendSMS(conf_list['sms_account_sid'], conf_list['sms_auth_token'], conf_list['sms_sender_number'], conf_list['sms_sender_recipient'], sms_msg)
